1446bf32c39ba70ceceed9bac6bf626b29dd49e0
Ramil Deocariza Jr. <ramildeocariza009@gmail.com>
2024-12-09 00:03:28 +0800

    Add files via upload

---
+++ Cellphone.java
@@ -0,0 +1,36 @@
+package practiceForFinals;
+
+public class Cellphone {
+
+	public static String brand;
+	public static double screenSize;
+	public static String color;
+	
+	
+	Cellphone(String brand, double screenSize, String color) {
+		this.brand = brand;
+		this.screenSize = screenSize;
+		this.color = color;
+	}
+	/*
+	public static void turnOn() {
+		System.out.println("Phone is on!");
+	}
+	
+	public static void turnOff() {
+		System.out.println("Phone is off!");
+	}
+	
+	public static void setBrand(String newBrand) {
+		brand = newBrand;
+	}
+	
+	public static void setScreenSize(double newScreenSize) {
+		screenSize = newScreenSize;
+	}
+	
+	public static void setColor(String newColor) {
+		color = newColor;
+	}
+	*/
+}
